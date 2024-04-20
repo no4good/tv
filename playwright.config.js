1b9b481c71e8483981810c2dc0b948d39cdacc8c
no4good <no4goodll@gmail.com>
2024-04-21 00:49:46 +0300

    feat: add test

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["list"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     actionTimeout: 300000,
