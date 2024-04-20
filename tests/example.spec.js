7fb45bc23f668e13be348a0d2aa61795cac90fbb
no4good <no4goodll@gmail.com>
2024-04-21 01:01:30 +0300

    test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,8 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
-
+console.log("process.env.2", process.env);
 test("has title", async ({ page }) => {
+  console.log("process.env.3", process.env);
   if (!process.env.URL) {
     throw new Error("URL is not defined");
   }
