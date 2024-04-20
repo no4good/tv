e3a190c46238c0c56e9945cf10efcc8e5c30f0d1
no4good <no4goodll@gmail.com>
2024-04-21 00:27:42 +0300

    feat: add test

---
+++ playwright.config.js
@@ -0,0 +1,34 @@
+// @ts-check
+const { defineConfig, devices } = require("@playwright/test");
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: "./tests",
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Retry on CI only */
+  retries: 0,
+  /* Opt out of parallel tests on CI. */
+  workers: 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: "html",
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    actionTimeout: 300000,
+    trace: "off",
+    headless: false,
+    channel: "chrome",
+    viewport: { width: 1600, height: 960 },
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+    screenshot: "only-on-failure",
+  },
+});
